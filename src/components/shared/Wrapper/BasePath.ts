975fd88dfe39c7096678ace4effbbbb192d896a6
Abdullah Ijaz <52850964+Abdullahijazvu@users.noreply.github.com>
2023-08-09 14:32:26 +0500

    change-base-path

--- src/components/shared/Wrapper/BasePath.ts
+++ src/components/shared/Wrapper/BasePath.ts
@@ -1,3 +1,3 @@
-const BASE_PATH_FORAPI = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://ecommerce-next-js-sanity-pied.vercel.app/"
+const BASE_PATH_FORAPI = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://ecommerce-next-js-sanity-pied.vercel.app"
 
 export default BASE_PATH_FORAPI
